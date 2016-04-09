8ba34599eec3967807b166c621c806985f8f2e45
David-Robinson <david.a.robinson87@gmail.com>
2016-04-09 10:52:27 -0400

    Shiny Files for Capstone Project
    Data Science Capstone Project files for Shiny application

--- ui.R
+++ ui.R
@@ -10,12 +10,11 @@ shinyUI(
             sidebarPanel(
                   
                   h4("Instructions"),
-                  p("Enter an incomplete phrase (series of multiple words) in the text box below 
+                  p("Enter an incomplete phrase (a series of multiple words) in the text box below 
                   and then select the 'Submit' button.  The phrase you input and the predicted next 
-                  word will appear in the text boxes on the right.  Since the model's database is 
-                  intentionally limited to minimize size and run time in order to provide a reasonable 
-                  user experience, if the 'predicted next word' doesn't appear or 'NA' appears, simply 
-                  type the next word in your phrase and press 'Enter'."),
+                  word will appear in the text boxes on the right.  The prediction model automatically 
+                  converts words to lower case and removes numbers, punctuation, and extra spaces.
+                  Thank you for your interest, and I hope you enjoy the application!"),
                                           
                   textInput('phrase', "Input a phrase (series of multiple words)",
                   placeholder = "type a series of words here"),
@@ -26,10 +25,9 @@ shinyUI(
             p("This application is a basic concept demonstration that uses Natural Language Processing 
             (NLP) to predict the 'next word' based on a series of words input by the user.  Some 
             compromises in accuracy have been accepted in order to create a small and fast enough model
-            to load into Shiny and run on shinyapps.io for the purposes of this demonstration project.  
-            The model uses dataframes containing 'tri-gram' (three-word) and 'bi-gram' (two-word) 
-            combinations sampled from blog, news, and Twitter files.  The prediction model automatically 
-            converts words to lower case and removes numbers, punctuation, and extra spaces."),
+            to load into Shiny, run on shinyapps.io, and provide a reasonable user experience for the 
+            purpose of this demonstration project.  The model uses dataframes containing 'tri-gram' 
+            (three-word) and 'bi-gram' (two-word) combinations sampled from blog, news, and Twitter files."),
             
             h3("You input the phrase:"),
             verbatimTextOutput('repeat_phrase'),
